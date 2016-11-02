7e8b31572be042dfa1203506dcca120e633f5f56
DockerDojo <dbellagio@us.ibm.com>
2016-11-02 12:07:21 -0400

    First commit

---
+++ src/main/java/com/ibm/microservices/refapp/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.ibm.microservices.refapp.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+                System.out.println("Starting " + HystrixDashboardApplication.class + " on Spring Cloud!");
+	}
+}
