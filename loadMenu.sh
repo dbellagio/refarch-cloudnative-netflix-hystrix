48a9cd666e5aff502fbdf4d84b36d79056e83993
DockerDojo <dbellagio@us.ibm.com>
2016-11-09 19:12:19 -0500

    moved or removed these

--- loadMenu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while [  0 -lt 1 ]; do
-      D=`date`
-      R=`curl -o /dev/null --silent -w "%{http_code} " "http://localhost:8181/" `;
-      echo $D $R;
-      sleep 1s;
-done
-
--- runlocal.sh
+++
@@ -1,2 +0,0 @@
-echo "Starting hystrix dashboard ........... listening on port 8383"
-docker run --name netflix-hysrix -p 8383:8383 --env-file ~/ibm-cloud-architecture/refarch-cloudnative-container-utils/env/env-eureka-only-local -d netflix-hystrix:latest
