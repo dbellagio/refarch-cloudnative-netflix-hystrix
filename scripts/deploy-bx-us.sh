06b5c3526d36a7e3b44f98a7cf83ff2f36e2f4de
DockerDojo <dbellagio@us.ibm.com>
2017-02-06 11:44:11 -0500

    Update path to env file

--- scripts/deploy-bx-us.sh
+++ scripts/deploy-bx-us.sh
@@ -10,7 +10,7 @@
 #  load balancer IP to set into the env files
 #  that use spring config only.
 #----------------------------------------------
-cd ~/ibm-cloud-architecture/refarch-cloudnative-netflix-hystrix
+# cd ~/ibm-cloud-architecture/refarch-cloudnative-netflix-hystrix/scripts
 # echo "create group for Eureka"
 cf ic group create --name netflix-eureka -p 8761 -m 512 --min 1 --max 2 --desired 1 registry.ng.bluemix.net/supercontainers/netflix-eureka:dev
 
@@ -27,7 +27,7 @@ echo "sleeping 120"
 sleep 120
 
 # echo "create group for the config server"
-cf ic group create --name spring-config -p 8888 -m 512 --min 1 --max 2 --desired 1 --env-file scripts/env-spring-config-remote registry.ng.bluemix.net/supercontainers/spring-config:dev
+cf ic group create --name spring-config -p 8888 -m 512 --min 1 --max 2 --desired 1 --env-file env-spring-config-remote registry.ng.bluemix.net/supercontainers/spring-config:dev
 
 echo "Perform a 'cf ic ps -a' to get the container ID of Config Server"
 echo "Perform a 'cf ic inspect' on the container ID to find the Load Balancer IP."
@@ -37,45 +37,45 @@ sleep 120
 
 # echo "create group for the backend micro services and pass in config server load balancer IP, Config Server has Eureka info and everything else in it"
 # echo "appetizer"
-cf ic group create --name appetizer -p 8082 -m 512 --min 1 --max 2 --desired 1 --env-file scripts/env-only-spring-config-remote registry.ng.bluemix.net/supercontainers/wfd-appetizer:dev
+cf ic group create --name appetizer -p 8082 -m 512 --min 1 --max 2 --desired 1 --env-file env-only-spring-config-remote registry.ng.bluemix.net/supercontainers/wfd-appetizer:dev
 
 echo "sleeping 10"
 sleep 10 
 
 echo "dessert"
-cf ic group create --name dessert -p 8083 -m 256 --min 1 --max 2 --desired 1 --env-file scripts/env-only-spring-config-remote registry.ng.bluemix.net/supercontainers/wfd-dessert:dev
+cf ic group create --name dessert -p 8083 -m 256 --min 1 --max 2 --desired 1 --env-file env-only-spring-config-remote registry.ng.bluemix.net/supercontainers/wfd-dessert:dev
 
 echo "sleeping 10"
 sleep 10
 
 echo "entree"
-cf ic group create --name entree -p 8081 -m 256 --min 1 --max 2 --desired 1 --env-file scripts/env-only-spring-config-remote registry.ng.bluemix.net/supercontainers/wfd-entree:dev
+cf ic group create --name entree -p 8081 -m 256 --min 1 --max 2 --desired 1 --env-file env-only-spring-config-remote registry.ng.bluemix.net/supercontainers/wfd-entree:dev
 
 echo "sleeping 120"
 sleep 120
 
 echo "create group for menu, pass in Eureka"
-cf ic group create --name menu -p 8180 -m 512 --min 1 --max 2 --desired 1 --env-file scripts/env-eureka-only-remote registry.ng.bluemix.net/supercontainers/wfd-menu:dev
+cf ic group create --name menu -p 8180 -m 512 --min 1 --max 2 --desired 1 --env-file env-eureka-only-remote registry.ng.bluemix.net/supercontainers/wfd-menu:dev
 
 echo "sleeping 60"
 sleep 60
 
 echo "create group for wfd-ui, pass in Eureka"
-cf ic group create --name wfd-ui -p 8181 -m 512 --min 1 --max 2 --desired 1 --env-file scripts/env-eureka-only-remote registry.ng.bluemix.net/supercontainers/wfd-ui:dev
+cf ic group create --name wfd-ui -p 8181 -m 512 --min 1 --max 2 --desired 1 --env-file env-eureka-only-remote registry.ng.bluemix.net/supercontainers/wfd-ui:dev
 
 echo "sleeping 60"
 sleep 60
 
 echo "Note: running one container for Turbine to bypass Cloud Foundry networking issues with Long Polling"
 echo "create single container for turbine, pass in Eureka"
-cf ic run --name netflix-turbine -p 8989 -m 512 --env-file scripts/env-eureka-only-remote registry.ng.bluemix.net/supercontainers/netflix-turbine:dev
+cf ic run --name netflix-turbine -p 8989 -m 512 --env-file env-eureka-only-remote registry.ng.bluemix.net/supercontainers/netflix-turbine:dev
 # cf ic group create --name netflix-turbine -p 8989 -m 512 --min 1 --max 2 --desired 1 --env-file env-eureka-only-remote registry.ng.bluemix.net/supercontainers/netflix-turbine:dev
 
 echo "sleeping 60"
 sleep 60
 
 echo "create group for zuul, pass in Eureka"
-cf ic group create --name netflix-zuul -p 8080 -m 512 --auto --min 1 --max 2 --desired 1 --env-file scripts/env-zuul-remote registry.ng.bluemix.net/supercontainers/netflix-zuul:dev
+cf ic group create --name netflix-zuul -p 8080 -m 512 --auto --min 1 --max 2 --desired 1 --env-file env-zuul-remote registry.ng.bluemix.net/supercontainers/netflix-zuul:dev
 
 echo "sleeping 60"
 sleep 60
@@ -83,8 +83,8 @@ sleep 60
 echo "Note: running one container for Hystrix to bypass Cloud Foundry networking issues with Long Polling"
 echo "create single container for Hystrix, pass in Eureka"
 # echo "create group for Hystrix, pass in Eureka"
-# cf ic group create --name netflix-hystrix -p 8383 -m 512 --min 1 --max 2 --desired 1 --env-file scripts/env-eureka-only-remote registry.ng.bluemix.net/supercontainers/netflix-hystrix:dev
-cf ic run --name netflix-hystrix -p 8383 -m 512 --env-file scripts/env-eureka-only-remote registry.ng.bluemix.net/supercontainers/netflix-hystrix:dev
+# cf ic group create --name netflix-hystrix -p 8383 -m 512 --min 1 --max 2 --desired 1 --env-file env-eureka-only-remote registry.ng.bluemix.net/supercontainers/netflix-hystrix:dev
+cf ic run --name netflix-hystrix -p 8383 -m 512 --env-file env-eureka-only-remote registry.ng.bluemix.net/supercontainers/netflix-hystrix:dev
 
 echo "create a public route to zuul for WFD UI App endpoint"
 cf create-route dev mybluemix.net --hostname super-zuul-dev --path whats-for-dinner
