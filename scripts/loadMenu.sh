b89360f7e2291a87e4e98db7e24c3a7b58498dee
DockerDojo <dbellagio@us.ibm.com>
2017-02-06 15:42:44 -0500

    updated config files

--- scripts/loadMenu.sh
+++ scripts/loadMenu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while [  0 -lt 1 ]; do
       D=`date`
-      R=`curl -o /dev/null --silent -w "%{http_code} " "http://localhost:8181/" `;
+      R=`curl -o /dev/null --silent -w "%{http_code} " "http://localhost:8080/whats-for-dinner" `;
       echo $D $R;
       sleep 1s;
 done
