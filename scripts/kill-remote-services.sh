ba8600f1ac9945ed5593503e27d27597f4195d56
DockerDojo <dbellagio@us.ibm.com>
2017-02-06 09:24:55 -0500

    add file to remote routes

--- scripts/kill-remote-services.sh
+++ scripts/kill-remote-services.sh
@@ -2,10 +2,16 @@ echo "Stopping zuul"
 cf ic group rm netflix-zuul
 
 echo "Stopping Hystrix Dashboard"
-cf ic group rm netflix-hystrix
+# cf ic group rm netflix-hystrix
+cf ic stop netflix-hystrix
+sleep 10
+cf ic rm netflix-hystrix
 
 echo "Stopping Turbine"
-cf ic group rm netflix-turbine
+# cf ic group rm netflix-turbine
+cf ic stop netflix-turbine
+sleep 10
+cf ic rm netflix-turbine
 
 echo "Stopping menu service"
 cf ic group rm menu 
